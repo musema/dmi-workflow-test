5ec966d21f6adbf798db0b137e6cd0f3d3af3e0e
Musema.Hassen <Musema.Hassen@gmail.com>
2018-04-24 23:44:47 -0400

    Initial commit

---
+++ src/main/java/com/dmi/maven/plugins/i_use_dmi_release_plugin/App.java
@@ -0,0 +1,13 @@
+package com.dmi.maven.plugins.i_use_dmi_release_plugin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
